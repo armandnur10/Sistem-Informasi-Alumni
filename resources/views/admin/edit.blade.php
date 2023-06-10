a8aea704763b721ac53656985ad72b2c4bc95eee
armandnur10 <zxuanggg@gmail.com>
2023-06-10 09:51:31 +0700

    commit

--- resources/views/admin/edit.blade.php
+++ resources/views/admin/edit.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Sistem Informasi Alumni</title>
     <script src="https://cdn.ckeditor.com/4.20.2/standard-all/ckeditor.js"></script>
 
     <!-------- Bootstrap -------->
